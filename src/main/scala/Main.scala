0614bef49b641f27d4420ad07b048fd4ccb631e7
Alejandro Alcalde <algui91@gmail.com>
2016-11-07 10:21:31 +0100

    Added log and style, closes #2

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2016 Alejandro Alcalde
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+  * Created by Alejandro Alcalde <contacto@elbauldelprogramador.com> on 11/7/16.
+  */
+object Main extends App {
+  val logger = org.log4s.getLogger
+
+  logger.info("Starting app")
+}
